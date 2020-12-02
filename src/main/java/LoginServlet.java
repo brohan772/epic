fedbf154bfefcd7cef9d0b5abbcc8c730640d619
Glassy1982 <sydrther@gmail.com>
2020-12-02 07:22:15 +0000

    Updated login success message

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -32,13 +32,16 @@ public class LoginServlet extends HttpServlet {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
             System.out.println("Connected to database");
-            String query = "SELECT * FROM users WHERE username=?";
+            String query = "SELECT * FROM users WHERE username=? AND pwd=? AND userrole=?";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, user);
+            stmt.setString(2,pass);
+            stmt.setString(3,role);
             ResultSet rs = stmt.executeQuery();
 
             if(rs.next()){
-                session.setAttribute("username", user);
+                session.setAttribute("username", user +
+                        ", you have successfully logged in!");
             }
 
             RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
